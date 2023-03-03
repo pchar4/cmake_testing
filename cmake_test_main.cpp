9a084f99f4319b03e67d8277f1210da8be02a817
pchar4 <pchar4@gmail.com>
2023-03-03 10:30:49 -0600

    more errors

--- cmake_test_main.cpp
+++ cmake_test_main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-#include "time_test.h"
+#include <chrono>
+// #include "time_test.h"
+using namespace std::chrono;
 
 int main(int, char**) {
-    time_test();
-    std::cout << "Hello, world!\n";
+    // time_test();
+    // std::cout << "Hello, world!\n";
+
+
+    // Recording the timestamp at the start of the code
+    auto beg = high_resolution_clock::now();
+ 
+    // Actual program code goes below this
+    // For demonstration a loop is ran which goes from 0 to
+    // 50000
+    for (int i = 0; i < 50000; i++)
+        continue;
+ 
+    // Taking a timestamp after the code is ran
+    auto end = high_resolution_clock::now();
+ 
+    // Subtracting the end timestamp from the beginning
+    // And we choose to receive the difference in
+    // microseconds
+    auto duration = duration_cast<microseconds>(end - beg);
+ 
+    // Displaying the elapsed time
+    std::cout << "Elapsed Time: " << duration.count();
+    return 0;
 }
+
